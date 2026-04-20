b4c36626451f1cfd7bff6a5420071feeea11df1a
fourierrr <fourierrr@gmail.com>
2026-04-20 23:55:39 +0800

    refactor: upgrade lazyvim to v15

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,41 +5,5 @@ return {
       -- Avoid startup warnings from which-key v3 rejecting Snacks' `real` field.
       which_key = false,
     },
-    dashboard = {
-      preset = {
-        --        header = [[
-        --        ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-        --        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
-        --        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
-        --        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
-        --        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
-        --        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
-        -- ]],
-        header = [[
-                                                        
-      █████╗ ██╗     ██╗██████╗  █████╗ ██████╗  █████╗ 
-     ██╔══██╗██║     ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗
-     ███████║██║     ██║██████╔╝███████║██████╔╝███████║
-     ██╔══██║██║     ██║██╔══██╗██╔══██║██╔══██╗██╔══██║
-     ██║  ██║███████╗██║██████╔╝██║  ██║██████╔╝██║  ██║
-     ╚═╝  ╚═╝╚══════╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝
-                                                        
-      ]],
-        -- stylua: ignore
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "o", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          -- { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "c", desc = "Config", action = ":e ~/.config/nvim/init.lua" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-      },
-    },
   },
 }
